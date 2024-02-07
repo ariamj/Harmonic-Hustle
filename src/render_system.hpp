ed6e42f6b8e2a7db1dd99859c3df0844f02876b3
brocereal <bcereal@students.cs.ubc.ca>
2024-02-07 13:50:35 -0800

    wrong order oops

--- src/render_system.hpp
+++ src/render_system.hpp
@@ -30,8 +30,8 @@ class RenderSystem {
 
 	// Make sure these paths remain in sync with the associated enumerators.
 	const std::array<std::string, texture_count> texture_paths =  {
-		textures_path("Enemy.png"),
-		textures_path("Player.png") };
+		textures_path("Player.png"),
+		textures_path("Enemy.png")};
 
 	std::array<GLuint, effect_count> effects;
 	// Make sure these paths remain in sync with the associated enumerators.
