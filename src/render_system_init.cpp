167116fba575191fca37a1e39d6d0e6de52fda47
tc2780 <tracyc2780@gmail.com>
2024-03-09 23:48:25 -0800

    update font path

--- src/render_system_init.cpp
+++ src/render_system_init.cpp
@@ -82,7 +82,8 @@ bool RenderSystem::init(GLFWwindow* window_arg)
 	initializeGlGeometryBuffers();
 
 	// setup fonts
-	std::string font_filename = "./data/fonts/Kenney_Future.ttf";
+	// std::string font_filename = "../data/fonts/Kenney_Future.ttf";
+	std::string font_filename = data_path() + "/fonts/Kenney_Future.ttf";
 	unsigned int font_default_size = 48;
 	fontInit(*window, font_filename, font_default_size);
 
